df915c646ac89784efa4e686deefe76f4b034ff2
kkimwooo <kkimwooo@gmail.com>
2022-11-28 21:35:07 +0900

    feat:GetLabels From Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,46 @@
+import { useEffect, useState } from "react";
+import axiosInstance from "./api/axios";
+import labelRequests from "./api/labelRequests";
+
 function App() {
+  const [labels, setLabels] = useState<string[]>([]);
+  const [selectedLabel, setSelectedLabel] = useState<string | null>(null);
+
+  useEffect(() => {
+    getLabels();
+  }, []);
+
+  const getLabels = async () => {
+    await axiosInstance.get(labelRequests.getLabelList).then(async (res) => {
+      const labelsFromServer = await res.data;
+      setLabels(labelsFromServer.data);
+    });
+  };
+
+  const renderLabels = () => {
+    if (labels.length === 0) {
+      return <div>There are no labels</div>;
+    }
+    return labels.map((label) => {
+      return (
+        <div
+          key={label}
+          onClick={() => {
+            setSelectedLabel(label);
+          }}
+        >
+          {label}
+        </div>
+      );
+    });
+  };
+
+  const addLabel = () => {
+    const newLabel = "라벨" + (labels.length + 1);
+    //TODO : 중복 검사 필요
+    setLabels([...labels, newLabel]);
+  };
+
   return (
     <div>
       <nav>
@@ -6,7 +48,9 @@ function App() {
       </nav>
       <div style={{ display: "flex", height: "100vh" }}>
         <div style={{ flex: "20%", border: "1px solid" }}>
-          <h3>Labels</h3>
+          <h3>Labels({"MemoList.length"})</h3>
+          {renderLabels()}
+          <input type="button" value="Add Label" onClick={addLabel} />
         </div>
         <div style={{ width: "30%", border: "1px solid" }}>
           <h3>Memo List</h3>
@@ -20,3 +64,11 @@ function App() {
 }
 
 export default App;
+
+interface Memo {
+  title: string;
+  content: string;
+  labels: string[];
+  updatedAt: Date;
+  createdAt: Date;
+}
